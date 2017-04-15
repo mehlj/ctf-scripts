3622c039c34e402d14031f235200d2588e6a13cd
Justen Mehl <justenmehl12@gmail.com>
2017-04-15 13:50:33 -0400

    Add files via upload

---
+++ brute.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in {1..4096}
+do
+    FLAG="$(/problems/bd058e83a119c78a006543d23d9f6422/bashloop $i)"
+    if [ "$FLAG" != "Nope. Pick another number between 0 and 4096" ]
+    then
+        echo $FLAG
+    exit
+    fi
+done
+
